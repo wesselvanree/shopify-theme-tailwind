84ed4a596891c179fcd5c7599082d823077d2d04
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2021-10-26 15:22:53 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ require('dotenv').config()
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: ['./**/*.liquid', 'src/**/*'],
+    content: [
+      './**/*.liquid',
+      './src/**/*.tsx',
+      './src/**/*.ts',
+      './src/**/*.jsx', 
+      './src/**/*.js'
+     ],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
