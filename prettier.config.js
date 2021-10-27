f4950b39fbaee4699a6c55461062e87000fdc349
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2021-10-27 19:58:20 +0200

    add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  overrides: [
+    {
+      files: 'shopify/**/*.js',
+      options: {
+        semi: true,
+      },
+    },
+  ],
+}
