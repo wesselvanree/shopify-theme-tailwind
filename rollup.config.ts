f644a793787a69765697d55f42217a92ea3b8697
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2022-09-02 13:20:43 +0200

    fix infinite build loop in watch caused by postcss

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,21 +1,8 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import * as fg from 'fast-glob'
 import {defineConfig, Plugin} from 'rollup'
 import swc from 'rollup-plugin-swc'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import postcss from 'rollup-plugin-postcss'
-
-function globInput(): Plugin {
-  return {
-    name: 'glob-input',
-    options(options) {
-      if (typeof options.input !== 'string')
-        throw new Error('Please provide a glob string as input')
-
-      return {...options, input: fg.sync(options.input)}
-    },
-  }
-}
 
 export default defineConfig([
   {
@@ -27,17 +14,21 @@ export default defineConfig([
     external: [],
     plugins: [globInput(), commonjs(), resolve(), swc()],
   },
-  {
-    input: 'src/styles/*.css',
-    output: {
-      dir: 'shopify/assets',
-      entryFileNames: '[name].css',
-    },
-    plugins: [
-      globInput(),
-      postcss({
-        extract: true,
-      }),
-    ],
-  },
 ])
+
+function globInput(): Plugin {
+  return {
+    name: 'glob-input',
+    options(options) {
+      if (typeof options.input !== 'string' && !Array.isArray(options.input))
+        throw new Error(
+          'Please provide a glob string or list of glob strings as input in your rollup config',
+        )
+
+      const inputs =
+        typeof options.input === 'string' ? [options.input] : options.input
+
+      return {...options, input: inputs.flatMap(input => fg.sync(input))}
+    },
+  }
+}
